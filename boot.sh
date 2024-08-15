f3d17e0306cdaec3b85130f7568a412a5732e42b
alephSystem <maran.dogpunk@gmail.com>
2024-08-15 16:56:18 -0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,8 +1,11 @@
 #!/bin/bash 
 
 while ! mysqladmin ping -h db -u ragnarok --password=ragnarok --silent; do
+    echo "Aguardando o MySQL iniciar..."
     sleep 1
 done
+
+echo "MySQL está disponível. Iniciando servidores."
 
 ./login-server &
 ./char-server &
